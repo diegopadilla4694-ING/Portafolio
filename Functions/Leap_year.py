401a209f76f7e6b968f8109c31087937304ad06c
DiegING <diego.padilla4694@alumnos.udg.mx>
2026-06-09 18:39:10 -0600

    Leap_year.py upgrade

--- Functions/Leap_year.py
+++ Functions/Leap_year.py
@@ -1,12 +1,6 @@
 user_is_year = int(input("Please enter one year: "))
 
 def is_leap_year(year):
-    if year % 4 == 0:
-       return True
-    
-    else:
-       return False
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-
-leap = is_leap_year(user_is_year)
-print(leap)
+print(is_leap_year(user_is_year))
